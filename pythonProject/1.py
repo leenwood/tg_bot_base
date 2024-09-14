82b5015f5ffc27e87e8b2b57d75bce5ff1a53d18
BceMFredner <104418700+BceMFredner@users.noreply.github.com>
2024-09-15 01:10:07 +0500

    Update 1.py

--- pythonProject/1.py
+++ pythonProject/1.py
@@ -4,7 +4,7 @@ import telebot
 from datetime import datetime
 from telebot import types
 
-token1="6649149061:AAFoKJcvKMI4RCgZ9cpuYH40snpKT-BjRa4"
+token1="7545888045:AAFmKfXBbRPUyteypzDWSlCX5jCY_5aZe2k"
 bot=telebot.TeleBot(token=token1)
 
 def log(mes):
@@ -15,25 +15,34 @@ def log(mes):
 #команда старт с добавлением кнопок
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup=types.InlineKeyboardMarkup(row_width=1)
-    ite1=types.InlineKeyboardButton("пукпук💩",callback_data="item1")
-    ite2=types.InlineKeyboardButton("не пукпук❌💩",callback_data="item2")
-    markup.add(ite1,ite2)
-    msg=bot.send_message(message.chat.id,"Давай познакомимся?✨💋🥰",reply_markup=markup)
+    markup1=types.InlineKeyboardMarkup(row_width=1)
+    ite1=types.InlineKeyboardButton("пукпук💩",callback_data="1")
+    ite2=types.InlineKeyboardButton("не пукпук❌💩",callback_data="2")
+    markup1.add(ite1,ite2)
+    bot.send_message(message.chat.id,"Давай познакомимся?✨💋🥰",reply_markup=markup1)
     log(message)
 
-#должны обрабатываться кнопки но нихуя не работает почему то
-@bot.callback_query_handler(func=lambda call:True)
-def callback(call):
-    if call.message:
-        if call.data == "item1":
-            bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.msg.message_id,text="Ураура🥰🥰🥰 держи конфетку🍬")
+@bot.message_handler(commands=['idigulyat'])
+def idigulyat(message):
+    markup2 = types.InlineKeyboardMarkup()
+    button1 = types.InlineKeyboardButton("прекол", url='https://a9fm.github.io/lightshot')
+    markup2.add(button1)
+    bot.send_message(message.chat.id,"тут прэкол",reply_markup=markup2)
+    log(message)
+
+
+#должны обрабатываться кнопки но РАБОТАЕТ АААААААА, но пришлось убрать редактирование прошлого сообщение
+@bot.callback_query_handler(func=lambda call: True)
+def check_callback_data(call):
+        if call.data == "1":
+            bot.send_message(chat_id=call.message.chat.id,text="Ураура🥰🥰🥰 держи конфетку🍬")
             time.sleep(2)
-            bot.send_photo(chat_id=call.message.chat.id,photo="ура.jpg")
-        elif call.data=="item2":
-            bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.msg.message_id,text="АААААААААААААААА")
+            bot.send_photo(chat_id=call.message.chat.id,photo="https://imgur.com/a/1KIcDd8")
+        elif call.data=="2":
+            bot.send_message(chat_id=call.message.chat.id,text="АААААААААААААААА")
             time.sleep(2)
-            bot.send_photo(chat_id=call.message.chat.id, photo="неура.jpg")
+            bot.send_photo(chat_id=call.message.chat.id, photo="https://imgur.com/a/juzlon9")
+
 
 
 #обработка любых других соо кроме команд
@@ -42,6 +51,9 @@ def echo_message(message):
     bot.reply_to(message, message.text)
     print("юзер:",message.chat.id,"| соо:",message.text)
     log(message)
+
+
+
 
 while True:
     try:
